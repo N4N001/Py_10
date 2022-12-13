230c4c9d84dc168ac172a5027ec3b1f8e1a90652
N4N001 <N4N001@protonmail.com>
2022-12-13 14:56:02 +0100

    darth

--- jmena.py
+++ jmena.py
@@ -1,6 +1,6 @@
 name=""
 def najdijmeno(name):
-    print("Darth: ",open("names.txt", "r").read().count(name))
+    print(name,": ",open("names.txt", "r").read().count(name))
 najdijmeno("Darth")
 najdijmeno("Lea")
 najdijmeno("Luke")
